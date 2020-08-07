710d118b7690d4fe4effbc1c29c9aac404c0d977
Nikita Khoroshavtsev <nikitakhor@gmail.com>
2020-08-07 15:03:28 +0900

    Mark words as new/old and save them to inner files

---
+++ app/src/main/java/com/example/nikit/mypersonalapp/LearnWordsActivity.java
@@ -0,0 +1,13 @@
+package com.example.nikit.mypersonalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LearnWordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_words);
+    }
+}
